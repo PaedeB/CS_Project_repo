15282a7d5bdd8795718ebf16201e7b3d0617b5ae
PaedeB <patrick.y.bryner@gmail.com>
2026-04-01 17:22:02 +0200

    Test Py hinzugefügt

--- TEST.py
+++ TEST.py
@@ -1,1 +1,22 @@
-## Testsite
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+import requests
+import sqlite3
+import xml.etree.ElementTree as ET
+from datetime import datetime, timedelta
+
+
+import json
+import time
+
+print("test")
+
+
+## Funktioniert nicht
+import plotly.graph_objects as go
+import plotly.express as px
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelEncoder
